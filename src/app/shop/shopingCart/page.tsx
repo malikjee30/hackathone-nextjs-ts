e59a7102c45bb2ea1107c1f02e7c4afae104e722
ghulamnasir <ghulamnasir30@gmail.com>
2024-12-21 00:10:30 +0500

    commit

--- src/app/shop/shopingCart/page.tsx
+++ src/app/shop/shopingCart/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Image from 'next/image';
 import React, { useState } from 'react';
 
 const initialCartItems = [
@@ -58,7 +59,7 @@ const ShoppingCart: React.FC = () => {
             {cartItems.map((item, index) => (
               <tr key={index} className="border-b">
                 <td className="p-4 flex items-center">
-                  <img src={item.image} alt={item.name} className="w-16 h-16 object-cover rounded mr-4" />
+                  <Image width={1000} height={1000} src={item.image} alt={item.name} className="w-16 h-16 object-cover rounded mr-4" />
                   <span>{item.name}</span>
                 </td>
                 <td className="p-4">${item.price.toFixed(2)}</td>
